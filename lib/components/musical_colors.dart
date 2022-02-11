556361ae5a56c975c1f16464764a3a8c9f9c0fb3
Aurele ASSOGBA <aureleassogba@montfort.on.ca>
2022-02-11 01:09:33 -0500

    modification des couleurs, ajustement des conventions

--- lib/components/musical_colors.dart
+++ lib/components/musical_colors.dart
@@ -1,9 +1,59 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:velocity_x/velocity_x.dart';
+import 'package:flutter/painting.dart';
+
+/*/* CSS HEX */
+--russian-violet: #3c1642ff;
+--lavender-blush: #f5e6e8ff;
+--languid-lavender: #d5c6e0ff;
+--blue-bell: #aaa1c8ff;
+--purple-mountain-majesty: #967aa1ff;
+
+/* CSS HSL */
+--russian-violet: hsla(292, 50%, 17%, 1);
+--lavender-blush: hsla(352, 43%, 93%, 1);
+--languid-lavender: hsla(275, 30%, 83%, 1);
+--blue-bell: hsla(254, 26%, 71%, 1);
+--purple-mountain-majesty: hsla(283, 17%, 55%, 1);
+
+/* SCSS HEX */
+$russian-violet: #3c1642ff;
+$lavender-blush: #f5e6e8ff;
+$languid-lavender: #d5c6e0ff;
+$blue-bell: #aaa1c8ff;
+$purple-mountain-majesty: #967aa1ff;
+
+/* SCSS HSL */
+$russian-violet: hsla(292, 50%, 17%, 1);
+$lavender-blush: hsla(352, 43%, 93%, 1);
+$languid-lavender: hsla(275, 30%, 83%, 1);
+$blue-bell: hsla(254, 26%, 71%, 1);
+$purple-mountain-majesty: hsla(283, 17%, 55%, 1);
+
+/* SCSS RGB */
+$russian-violet: rgba(60, 22, 66, 1);
+$lavender-blush: rgba(245, 230, 232, 1);
+$languid-lavender: rgba(213, 198, 224, 1);
+$blue-bell: rgba(170, 161, 200, 1);
+$purple-mountain-majesty: rgba(150, 122, 161, 1);
+
+/* SCSS Gradient */
+$gradient-top: linear-gradient(0deg, #3c1642ff, #f5e6e8ff, #d5c6e0ff, #aaa1c8ff, #967aa1ff);
+$gradient-right: linear-gradient(90deg, #3c1642ff, #f5e6e8ff, #d5c6e0ff, #aaa1c8ff, #967aa1ff);
+$gradient-bottom: linear-gradient(180deg, #3c1642ff, #f5e6e8ff, #d5c6e0ff, #aaa1c8ff, #967aa1ff);
+$gradient-left: linear-gradient(270deg, #3c1642ff, #f5e6e8ff, #d5c6e0ff, #aaa1c8ff, #967aa1ff);
+$gradient-top-right: linear-gradient(45deg, #3c1642ff, #f5e6e8ff, #d5c6e0ff, #aaa1c8ff, #967aa1ff);
+$gradient-bottom-right: linear-gradient(135deg, #3c1642ff, #f5e6e8ff, #d5c6e0ff, #aaa1c8ff, #967aa1ff);
+$gradient-top-left: linear-gradient(225deg, #3c1642ff, #f5e6e8ff, #d5c6e0ff, #aaa1c8ff, #967aa1ff);
+$gradient-bottom-left: linear-gradient(315deg, #3c1642ff, #f5e6e8ff, #d5c6e0ff, #aaa1c8ff, #967aa1ff);
+$gradient-radial: radial-gradient(#3c1642ff, #f5e6e8ff, #d5c6e0ff, #aaa1c8ff, #967aa1ff);
+*/
 
 mixin MusicalColors {
-  static Color mBlueColor = Vx.blue400;
-  static Color mPurpleColor = Vx.purple400;
-  static Color mPurpleLColor = Vx.blue300;
-  static Color mBlackColor = Vx.black;
+  static const Color russianCioletColor = Color(0x3c1642ff);
+  static const Color lavenderBlush = Color(0xf5e6e8ff);
+  static const Color languidLavender = Color(0xd5c6e0ff);
+  static const Color blueBell = Color(0xaaa1c8ff);
+  static const Color purpleMountainMajesty = Color(0x967aa1ff);
 }
--- lib/screens/list_view_zik.dart
+++ lib/screens/list_view_zik.dart
@@ -1,7 +1,7 @@
 import 'package:alan_application_vocal_alimente/components/musical_colors.dart';
 import 'package:alan_application_vocal_alimente/model/channel.dart';
 import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -16,6 +16,7 @@ class ListViewZik extends StatefulWidget {
 class _ListViewZikState extends State<ListViewZik> {
   List<Channel> channels = [];
   late Channel selectedChannel;
+  late int idToPlay;
   bool isPlaying = false;
 
   final AudioPlayer audioPlayer = AudioPlayer();
@@ -26,6 +27,7 @@ class _ListViewZikState extends State<ListViewZik> {
     retriveChannels();
     audioPlayer.onPlayerStateChanged.listen((event) {
       setState(() {
+        // idToPlay = event.index;
         if (event == PlayerState.PLAYING) {
           isPlaying = true;
         } else {
@@ -46,8 +48,9 @@ class _ListViewZikState extends State<ListViewZik> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
+      // drawer: const Drawer(),
       appBar: AppBar(
+        backgroundColor: MusicalColors.blueBell,
         title: "Musical"
             .text
             .xl4
@@ -60,7 +63,12 @@ class _ListViewZikState extends State<ListViewZik> {
         // elevation: 0.0,
         actions: <Widget>[
           // ignore: prefer_const_constructors
-          IconButton(onPressed: () {}, icon: Icon(Icons.refresh))
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.refresh,
+                size: 30.0,
+              ))
         ],
         toolbarHeight: 60,
       ),
@@ -68,35 +76,42 @@ class _ListViewZikState extends State<ListViewZik> {
         VxAnimatedBox()
             // .size(context.screenWidth, context.screenHeight)
             .withGradient(
-              LinearGradient(
+              const LinearGradient(
                 colors: [
-                  MusicalColors.mPurpleColor,
-                  MusicalColors.mPurpleLColor,
+                  MusicalColors.russianCioletColor,
+                  MusicalColors.lavenderBlush,
+                  MusicalColors.languidLavender,
+                  MusicalColors.blueBell,
+                  MusicalColors.purpleMountainMajesty,
                 ],
                 begin: Alignment.topRight,
                 end: Alignment.bottomLeft,
               ),
             )
             .make(),
-        // AppBar(
-        //   title: "Musical".text.xl4.bold.white.make().shimmer(
-        //       primaryColor: Colors.yellow, secondaryColor: Colors.pink),
-        //   backgroundColor: Colors.transparent,
-        //   centerTitle: true,
-        //   elevation: 0.0,
-        //   actions: <Widget>[
-        //     // ignore: prefer_const_constructors
-        //     IconButton(onPressed: () {}, icon: Icon(Icons.refresh))
-        //   ],
-        // ).h(100).p16(),
         ListWheelScrollView.useDelegate(
+            squeeze: 0.8,
             itemExtent: 250,
+            onSelectedItemChanged: (index) => {
+                  setState(() {
+                    idToPlay = index;
+                    if (kDebugMode) {
+                      print(index);
+                    }
+                  })
+                },
             childDelegate: ListWheelChildBuilderDelegate(
               childCount: channels.length,
               builder: (context, index) {
                 return Container(
                   decoration: BoxDecoration(
-                    color: Colors.blue.shade200,
+                    border: (idToPlay == index)
+                        ? Border.all(
+                            color: MusicalColors.russianCioletColor,
+                            width: 6.0,
+                          )
+                        : Border.all(color: MusicalColors.lavenderBlush),
+                    color: MusicalColors.lavenderBlush,
                     borderRadius: const BorderRadius.all(Radius.circular(30.0)),
                   ),
                   child: Row(
@@ -105,64 +120,27 @@ class _ListViewZikState extends State<ListViewZik> {
                         child: Column(
                           children: [
                             ListTile(
-                              title: Text(isPlaying
-                                  ? "Playing now - ${channels[index].name.toUpperCase()}"
-                                  : channels[index].name.toUpperCase()),
+                              title: Text(
+                                (isPlaying && idToPlay == index)
+                                    ? "Playing now - ${channels[index].name.toUpperCase()} - $index"
+                                    : channels[index].name.toUpperCase(),
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
                               subtitle:
                                   Text(channels[index].genre.toLowerCase()),
                             ),
                             Container(
                               height: 150.0,
                               decoration: BoxDecoration(
-                                // color: Colors.red.shade200,
-
                                 image: DecorationImage(
                                   image: NetworkImage(channels[index].imageUrl),
                                   fit: BoxFit.fitHeight,
                                 ),
                               ),
-                              child: GestureDetector(
-                                onDoubleTap: () {
-                                  jouerMusic(channels[index].uri);
-                                },
-                              ),
                             )
                           ],
                         ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          CupertinoButton(
-                              // ignore: prefer_const_constructors
-                              child: Icon(
-                                CupertinoIcons.heart,
-                                size: 50,
-                                color: Colors.white,
-                              ),
-                              onPressed: () {}),
-                          // ignore: prefer_const_constructors
-                          // Divider(
-                          //   height: 2.0,
-                          //   color: Colors.white,
-                          // ),
-                          CupertinoButton(
-                              // ignore: prefer_const_constructors
-                              child: Icon(
-                                isPlaying
-                                    ? CupertinoIcons.stop_circle
-                                    : CupertinoIcons.play_circle,
-                                size: 50,
-                                color: Colors.white,
-                              ),
-                              onPressed: () {
-                                if (isPlaying) {
-                                  audioPlayer.stop();
-                                } else {
-                                  jouerMusic(channels[index].uri);
-                                }
-                              }),
-                        ],
                       ),
                     ],
                   ),
@@ -170,6 +148,57 @@ class _ListViewZikState extends State<ListViewZik> {
               },
             ))
       ]),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 5.0,
+        color: MusicalColors.purpleMountainMajesty,
+        child: IconTheme(
+          data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+          child: Row(
+            children: <Widget>[
+              const Spacer(),
+              IconButton(
+                tooltip: 'Open navigation menu',
+                icon: const Icon(
+                  Icons.menu,
+                  size: 40.0,
+                ),
+                onPressed: () {},
+              ),
+              const Spacer(
+                flex: 3,
+              ),
+              IconButton(
+                tooltip: 'play',
+                icon: Icon(
+                  isPlaying
+                      ? Icons.stop_circle_outlined
+                      : Icons.play_circle_outline,
+                  size: 40.0,
+                ),
+                onPressed: () {
+                  if (isPlaying) {
+                    audioPlayer.stop();
+                  } else {
+                    jouerMusic(channels[idToPlay].uri);
+                  }
+                },
+              ),
+              const Spacer(
+                flex: 3,
+              ),
+              IconButton(
+                tooltip: 'Favorite',
+                icon: const Icon(
+                  Icons.favorite_outline,
+                  size: 40.0,
+                ),
+                onPressed: () {},
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
