9c8a7681eecb52184801db6577a6db2e22563ef8
Yaseen Choukri <52813353+Yassotaha@users.noreply.github.com>
2022-02-11 23:38:31 -0500

    J'ai terminer l'interface maintenant manque radio

--- lib/DetailsMusique.dart
+++ lib/DetailsMusique.dart
@@ -22,6 +22,12 @@ class AppMusique extends StatefulWidget {
 }
 
 class _AppMusiqueState extends State<AppMusique> {
+  // Quelque Variables
+  bool playing = false; // aucune radio
+  IconData playBtn = Icons.play_arrow;
+
+  // Ici pour declarer les variable qu'on va utiliser pour notre music player
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,29 +43,131 @@ class _AppMusiqueState extends State<AppMusique> {
               ]),
         ),
         child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 48.0, horizontal: 12.0),
+          padding: EdgeInsets.only(
+            top: 48.0,
+          ),
           child: Container(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
+              children: [
                 // On va ajouter des texts pour les titres
-                Text(
-                  "Les Beats de la Musique",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 38.0,
-                    fontWeight: FontWeight.bold,
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: Text(
+                    "Beats de la Musique",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 38.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                Text(
-                  "Ecoute Ta musique prefere",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.w400,
+                Padding(
+                  padding: EdgeInsets.only(left: 12.0),
+                  child: Text(
+                    "Ecoute Ta musique prefere",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
+
+                SizedBox(
+                  height: 24.0,
+                ),
+
+                //Let's add the music cover
+                Center(
+                  child: Container(
+                    width: 280.0,
+                    height: 280.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        image: DecorationImage(
+                          image: AssetImage("assets/image.jpg"),
+                        )),
+                  ),
+                ),
+                SizedBox(
+                  height: 18.0,
+                ),
+                Center(
+                  child: Text(
+                    "Piece of your heart",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+
+                Expanded(
+                    child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // on commence par ajouter le controlleur
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            iconSize: 45.0,
+                            color: Colors.blue,
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.skip_previous,
+                            ),
+                          ),
+                          IconButton(
+                            iconSize: 62.0,
+                            color: Colors.blue[800],
+                            onPressed: () {
+                              // Ici sont les functionalites pour le button jouer ou Play
+                              if (!playing) {
+                                setState(() {
+                                  playBtn = Icons.pause;
+                                  playing = true;
+                                });
+                              } else {
+                                setState(() {
+                                  playBtn = Icons.play_arrow;
+                                  playing = false;
+                                });
+                              }
+                            },
+                            icon: Icon(
+                              playBtn,
+                            ),
+                          ),
+                          IconButton(
+                            iconSize: 45.0,
+                            color: Colors.blue,
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.skip_next,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ))
               ],
             ),
           ),
