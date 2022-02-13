65425e127fd2aa8546d80400e749410d007c0345
Yaseen Choukri <52813353+Yassotaha@users.noreply.github.com>
2022-02-13 13:08:02 -0500

    updates

--- lib/screens/detail_zik2.dart
+++ lib/screens/detail_zik2.dart
@@ -1,16 +1,15 @@
-//import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: AppMusique(),
     );
@@ -18,13 +17,17 @@ class MyApp extends StatelessWidget {
 }
 
 class AppMusique extends StatefulWidget {
-  const AppMusique({Key? key}) : super(key: key);
-
   @override
   _AppMusiqueState createState() => _AppMusiqueState();
 }
 
 class _AppMusiqueState extends State<AppMusique> {
+  // Quelque Variables
+  bool playing = false; // aucune radio
+  IconData playBtn = Icons.play_arrow;
+
+  // Ici pour declarer les variable qu'on va utiliser pour notre music player
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,29 +43,133 @@ class _AppMusiqueState extends State<AppMusique> {
               ]),
         ),
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 48.0, horizontal: 12.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              // On va ajouter des texts pour les titres
-              Text(
-                "Les Beats de la Musique",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 38.0,
-                  fontWeight: FontWeight.bold,
+          padding: EdgeInsets.only(
+            top: 48.0,
+          ),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // On va ajouter des texts pour les titres
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: Text(
+                    "Beats de la Musique",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 38.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
-              Text(
-                "Ecoute Ta musique prefere",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w400,
+                Padding(
+                  padding: EdgeInsets.only(left: 12.0),
+                  child: Text(
+                    "Ecoute Ta musique prefere",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
                 ),
-              ),
-            ],
+
+                SizedBox(
+                  height: 24.0,
+                ),
+
+                //Let's add the music cover
+                Center(
+                  child: Container(
+                    width: 280.0,
+                    height: 280.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        image: DecorationImage(
+                          image: AssetImage("assets/image.jpg"),
+                        )),
+                  ),
+                ),
+                SizedBox(
+                  height: 18.0,
+                ),
+                Center(
+                  child: Text(
+                    "Piece of your heart",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+
+                Expanded(
+                    child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // on commence par ajouter le controlleur
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            iconSize: 45.0,
+                            color: Colors.blue,
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.skip_previous,
+                            ),
+                          ),
+                          IconButton(
+                            iconSize: 62.0,
+                            color: Colors.blue[800],
+                            onPressed: () {
+                              // Ici sont les functionalites pour le button jouer ou Play
+                              if (!playing) {
+                                setState(() {
+                                  playBtn = Icons.pause;
+                                  playing = true;
+                                });
+                              } else {
+                                setState(() {
+                                  playBtn = Icons.play_arrow;
+                                  playing = false;
+                                });
+                              }
+                            },
+                            icon: Icon(
+                              playBtn,
+                            ),
+                          ),
+                          IconButton(
+                            iconSize: 45.0,
+                            color: Colors.blue,
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.skip_next,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ))
+              ],
+            ),
           ),
         ),
       ),
