1769a3ec1def1d6095c51f975691a4be902a9a23
Yaseen Choukri <52813353+Yassotaha@users.noreply.github.com>
2022-03-20 16:45:34 -0400

    Fixed some bugs and new changes

--- lib/providers/radio_control_provider.dart
+++ lib/providers/radio_control_provider.dart
@@ -2,18 +2,29 @@ import 'package:alan_voice/alan_voice.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 import '../model/channel.dart';
 
 class RadioControlNotifier extends ChangeNotifier {
   late List<Channel> channels = [];
   late Channel selectedChannel;
+  late Channel _chaineChoisi;
   late int _idToPlay = 0;
   bool _isPlaying = false;
 
   late List<Channel> _favoris = [];
 
   List<Channel> get favoris => _favoris;
+  var _ctxt;
+
+  get ctxt => _ctxt;
+
+  set ctxt(newCtxt) {
+    _ctxt = newCtxt;
+    notifyListeners();
+  }
+
   List<Channel> addFavoris(int id) {
     _favoris.add(channels[id]);
     notifyListeners();
@@ -21,6 +32,12 @@ class RadioControlNotifier extends ChangeNotifier {
   }
 
   final AudioPlayer audioPlayer = AudioPlayer();
+  Channel get chaineChoisi => _chaineChoisi;
+
+  set chaineChoisi(Channel cc) {
+    _chaineChoisi = cc;
+    notifyListeners();
+  }
 
   int get idToPlay => _idToPlay;
   set idToPlay(int id) {
@@ -36,13 +53,34 @@ class RadioControlNotifier extends ChangeNotifier {
     AlanVoice.callbacks.add((command) => _handleCommand(command.data));
   }
 
+  navigation(ctxt) {
+    Navigator.pushNamed(ctxt, '/second');
+  }
+
   _handleCommand(Map<String, dynamic> response) {
     switch (response["command"]) {
-      case "play":
-        jouerMusic(selectedChannel.url);
+      case "play": // dans le cas l'utilisateur est anglophone
+        jouerMusic(chaineChoisi.url);
+        navigation(ctxt);
+        break;
+      case "Jouer":
+        jouerMusic(
+            chaineChoisi.url); // dans le cas l'utilisateur est francophone
+        break;
+      case "stop": // dans le cas l'utilisateur veut arrete la musique
+        audioPlayer.stop();
+        break;
+      case "next": // On veut jour le prochain channel
+        jouerNext(chaineChoisi.id);
+
+        break;
+      case "prev": // On veut jour le channel precedente
+        jouerPrev(chaineChoisi.id);
+        //navigation(ctxt);
         break;
       default:
         print("Command was ${response["command"]}");
+        break;
     }
   }
 
@@ -83,7 +121,9 @@ class RadioControlNotifier extends ChangeNotifier {
       notifyListeners();
     } else {}
     id++;
+    idToPlay = id;
     jouerMusic(channels[id].url);
+    //navigation(ctxt);
     notifyListeners();
   }
 
@@ -93,6 +133,7 @@ class RadioControlNotifier extends ChangeNotifier {
       notifyListeners();
     } else {}
     id--;
+    idToPlay = id;
     jouerMusic(channels[id].url);
     notifyListeners();
   }
--- lib/screens/detail_zik.dart
+++ lib/screens/detail_zik.dart
@@ -1,222 +1,204 @@
+import 'package:alan_application_vocal_alimente/providers/radio_control_provider.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 import '../components/musical_colors.dart';
 import '../model/channel.dart';
 
 class DetailZik extends StatelessWidget {
-  int idPlay = 0;
-  final AudioPlayer audioPlayer = AudioPlayer();
-  List<Channel> channels = [];
-  bool isPlaying = false;
-  int cle = 0;
-  // Quelque Variables
-  //bool playing = true; // aucune radio
-  IconData playBtn = Icons.play_arrow;
-
-  // DetailZik(id, {Key? key}) : super(key: key);
-  //List<Channel> channels = [];
-  // late int idPlay;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: MusicalColors.blueBell,
-          title: "Musical".text.xl4.bold.white.make().shimmer(
-              primaryColor: Colors.yellow, secondaryColor: Colors.pink),
-          //backgroundColor: Colors.transparent,
-          centerTitle: true,
-          // elevation: 0.0,
-          actions: <Widget>[
-            // ignore: prefer_const_constructors
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.refresh,
-                  size: 30.0,
-                ))
-          ],
-          toolbarHeight: 60,
-        ),
-
-        // Ici on cree l'interface principale de l'application
-        body: Stack(children: [
-          VxAnimatedBox()
-              // .size(context.screenWidth, context.screenHeight)
-              .withGradient(
-                const LinearGradient(
-                  colors: [
-                    MusicalColors.russianCioletColor,
-                    MusicalColors.lavenderBlush,
-                    MusicalColors.languidLavender,
-                    MusicalColors.blueBell,
-                    MusicalColors.purpleMountainMajesty,
-                  ],
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                ),
-              )
-              .make(),
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Colors.blue[800]!,
-                    Colors.blue[200]!,
-                  ]),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                top: 48.0,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // On va ajouter des texts pour les titres
-                  Padding(
-                    padding: const EdgeInsets.only(left: 12.0),
-                    child: Text(
-                      channels[cle]
-                          .genre
-                          .toUpperCase(), // recuepere le genre de chaque radio
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 38.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 12.0),
-                    child: Text(
-                      "Ecoute Ta radio préférée",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-
-                  const SizedBox(
-                    height: 24.0,
-                  ),
-
-                  //Let's add the music cover
-                  Center(
-                    child: Container(
-                      width: 280.0,
-                      height: 280.0,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30.0),
-                          image: DecorationImage(
-                            image: NetworkImage(channels[cle]
-                                .imageUrl), // recuepere les images de chaque radio
-                          )),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 18.0,
-                  ),
-                  Center(
-                    child: Text(
-                      channels[cle]
-                          .name
-                          .toUpperCase(), // recuepere les noms de chaque radio
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 32.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30.0,
-                  ),
-
-                  Expanded(
-                      child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30.0),
-                        topRight: Radius.circular(30.0),
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        // on commence par ajouter le controlleur
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            IconButton(
-                              iconSize: 45.0,
-                              color: Colors.blue,
-                              onPressed: () {
-                                if (idPlay > 0) {
-                                  cle = idPlay - 1;
-                                }
-
-                                audioPlayer.play(channels[cle].url);
-                              },
-                              icon: const Icon(
-                                Icons
-                                    .skip_previous, // Button pour une radio precedente
-                              ),
-                            ),
-                            IconButton(
-                              icon: Icon(
-                                isPlaying ? Icons.pause : Icons.play_arrow,
-                                size: 40.0,
-                              ),
-                              iconSize: 62.0,
-                              color: Colors.blue[800],
-                              onPressed: () {
-                                // Ici sont les functionalites pour le button jouer ou Play
-                                if (isPlaying) {
-                                  //playBtn = Icons.pause;
-                                  audioPlayer.stop();
-                                  //isPlaying = false;
-                                } else {
-                                  //playBtn = Icons.play_arrow;
-
-                                  audioPlayer.play(channels[cle].url);
-
-                                  //isPlaying = true;
-                                }
-                              },
-                            ),
-                            IconButton(
-                              iconSize: 45.0,
-                              color: Colors.blue,
-                              onPressed: () {
-                                if (idPlay < 49) {
-                                  cle = idPlay + 1;
-                                }
-                                audioPlayer.play(channels[cle].url);
-                              },
-                              icon: const Icon(
-                                Icons
-                                    .skip_next, // Button pour une radio suivante
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
+    return Consumer<RadioControlNotifier>(builder: (context, value, child) {
+      return Scaffold(
+          appBar: AppBar(
+            backgroundColor: MusicalColors.blueBell,
+            title: "Musical".text.xl4.bold.white.make().shimmer(
+                primaryColor: Colors.yellow, secondaryColor: Colors.pink),
+            //backgroundColor: Colors.transparent,
+            centerTitle: true,
+            // elevation: 0.0,
+            actions: <Widget>[
+              // ignore: prefer_const_constructors
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.refresh,
+                    size: 30.0,
                   ))
-                ],
+            ],
+            toolbarHeight: 60,
+          ),
+
+          // Ici on cree l'interface principale de l'application
+          body: Stack(children: [
+            VxAnimatedBox()
+                // .size(context.screenWidth, context.screenHeight)
+                .withGradient(
+                  const LinearGradient(
+                    colors: [
+                      MusicalColors.russianCioletColor,
+                      MusicalColors.lavenderBlush,
+                      MusicalColors.languidLavender,
+                      MusicalColors.blueBell,
+                      MusicalColors.purpleMountainMajesty,
+                    ],
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                  ),
+                )
+                .make(),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Colors.blue[800]!,
+                      Colors.blue[200]!,
+                    ]),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 48.0,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // On va ajouter des texts pour les titres
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12.0),
+                      child: Text(
+                        value.channels[value.idToPlay].genre
+                            .toUpperCase(), // recuepere le genre de chaque radio
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 12.0),
+                      child: Text(
+                        "Ecoute Ta radio préférée",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(
+                      height: 24.0,
+                    ),
+
+                    //Let's add the music cover
+                    Center(
+                      child: Container(
+                        width: 280.0,
+                        height: 280.0,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30.0),
+                            image: DecorationImage(
+                              image: NetworkImage(value.channels[value.idToPlay]
+                                  .imageUrl), // recuepere les images de chaque radio
+                            )),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 18.0,
+                    ),
+                    Center(
+                      child: Text(
+                        value.channels[value.idToPlay].name
+                            .toUpperCase(), // recuepere les noms de chaque radio
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+
+                    Expanded(
+                        child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30.0),
+                          topRight: Radius.circular(30.0),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          // on commence par ajouter le controlleur
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                iconSize: 45.0,
+                                color: Colors.blue,
+                                onPressed: () {
+                                  value.jouerPrev(value.idToPlay);
+                                },
+                                icon: const Icon(
+                                  Icons
+                                      .skip_previous, // Button pour une radio precedente
+                                ),
+                              ),
+                              IconButton(
+                                icon: Icon(
+                                  value.isPlaying()
+                                      ? Icons.pause
+                                      : Icons.play_arrow,
+                                  size: 40.0,
+                                ),
+                                iconSize: 62.0,
+                                color: Colors.blue[800],
+                                onPressed: () {
+                                  // Ici sont les functionalites pour le button jouer ou Play
+                                  if (value.isPlaying()) {
+                                    value.audioPlayer.stop();
+                                  } else {
+                                    //playBtn = Icons.play_arrow;
+                                    value.jouerMusic(
+                                        value.channels[value.idToPlay].url);
+
+                                    //isPlaying = true;
+                                  }
+                                },
+                              ),
+                              IconButton(
+                                iconSize: 45.0,
+                                color: Colors.blue,
+                                onPressed: () {
+                                  value.jouerNext(value.idToPlay);
+                                },
+                                icon: const Icon(
+                                  Icons
+                                      .skip_next, // Button pour une radio suivante
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ))
+                  ],
+                ),
               ),
             ),
-          ),
-        ]));
+          ]));
+    });
   }
 }
